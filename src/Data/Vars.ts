cf2381675502b6c9593bafddb1b6e28e42b00ed9
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-10-25 11:47:27 +0700

    feat: Sorters id

--- src/Data/Vars.ts
+++ src/Data/Vars.ts
@@ -5,3 +5,22 @@ export enum Gamemode {
 	PVP,
 	Editor,
 }
+
+export enum SorterID {
+	Copper = 0,
+	Lead = 1,
+	Metaglass = 2,
+	Graphite = 3,
+	Sand = 4,
+	Coal = 5,
+	Titanium = 6,
+	Thorium = 7,
+	Scrap = 8,
+	Silicon = 9,
+	Plastanium = 10,
+	PhaseFabric = 11,
+	SurgeAlloy = 12,
+	SporePod = 13,
+	BlastCompound = 14,
+	Pyratite = 15,
+}

35c32720ad54eff1d9da0ee320d396e3ae894a7d
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-10-25 14:37:44 +0700

    feat: Dynamic buffer

--- src/CustomBuffer.ts
+++ src/CustomBuffer.ts
@@ -2,9 +2,13 @@ import { MUtf8Decoder } from 'mutf-8'
 
 class CustomBuffer {
 	#offset: number = 0
+	#length: number = 0
 	#buffer: Buffer = Buffer.alloc(0)
+	#capacity: number = 0
 
 	read(): number {
+		this.checkCanRead(1)
+
 		const data = this.#buffer.readInt8(this.#offset)
 
 		this.#offset += 1
@@ -13,6 +17,8 @@ class CustomBuffer {
 	}
 
 	readShort(): number {
+		this.checkCanRead(2)
+
 		const data = this.#buffer.readInt16BE(this.#offset)
 
 		this.#offset += 2
@@ -21,6 +27,8 @@ class CustomBuffer {
 	}
 
 	readInt(): number {
+		this.checkCanRead(4)
+
 		const data = this.#buffer.readInt32BE(this.#offset)
 
 		this.#offset += 4
@@ -29,6 +37,8 @@ class CustomBuffer {
 	}
 
 	readLong(): bigint {
+		this.checkCanRead(8)
+
 		const data = this.#buffer.readBigInt64BE(this.#offset)
 
 		this.#offset += 8
@@ -37,6 +47,8 @@ class CustomBuffer {
 	}
 
 	readFloat(): number {
+		this.checkCanRead(4)
+
 		const data = this.#buffer.readFloatBE(this.#offset)
 
 		this.#offset += 4
@@ -45,6 +57,8 @@ class CustomBuffer {
 	}
 
 	readDouble(): number {
+		this.checkCanRead(8)
+
 		const data = this.#buffer.readDoubleBE(this.#offset)
 
 		this.#offset += 8
@@ -53,6 +67,8 @@ class CustomBuffer {
 	}
 
 	readUByte(): number {
+		this.checkCanRead(1)
+
 		const data = this.#buffer.readUint8(this.#offset)
 
 		this.#offset += 1
@@ -61,6 +77,8 @@ class CustomBuffer {
 	}
 
 	readUShort(): number {
+		this.checkCanRead(2)
+
 		const data = this.#buffer.readUint16BE(this.#offset)
 
 		this.#offset += 2
@@ -79,6 +97,8 @@ class CustomBuffer {
 
 		const textBuffer: CustomBuffer = CustomBuffer.alloc(length)
 
+		this.checkCanRead(length)
+
 		for (let i = 0; i < length; i++) {
 			textBuffer.write(this.read())
 		}
@@ -91,51 +111,75 @@ class CustomBuffer {
 	}
 
 	write(value: number): void {
+		this.checkExpand(1)
+
 		this.#buffer.writeInt8(value, this.#offset)
 
 		this.#offset += 1
+		this.#length += 1
 	}
 
 	writeShort(value: number): void {
+		this.checkExpand(2)
+
 		this.#buffer.writeInt16BE(value, this.#offset)
 
 		this.#offset += 2
+		this.#length += 2
 	}
 
 	writeInt(value: number): void {
+		this.checkExpand(4)
+
 		this.#buffer.writeInt32BE(value, this.#offset)
 
 		this.#offset += 4
+		this.#length += 4
 	}
 
 	writeLong(value: bigint): void {
+		this.checkExpand(8)
+
 		this.#buffer.writeBigInt64BE(value, this.#offset)
 
 		this.#offset += 8
+		this.#length += 8
 	}
 
 	writeFloat(value: number): void {
+		this.checkExpand(4)
+
 		this.#buffer.writeFloatBE(value, this.#offset)
 
 		this.#offset += 4
+		this.#length += 4
 	}
 
 	writeDouble(value: number): void {
+		this.checkExpand(8)
+
 		this.#buffer.writeDoubleBE(value, this.#offset)
 
 		this.#offset += 8
+		this.#length += 8
 	}
 
 	writeUByte(value: number): void {
+		this.checkExpand(1)
+
 		this.#buffer.writeUInt8(value, this.#offset)
 
 		this.#offset += 1
+		this.#length += 1
 	}
 
 	writeUShort(value: number): void {
+		this.checkExpand(2)
+
 		this.#buffer.writeUInt16BE(value, this.#offset)
 
 		this.#offset += 2
+		this.#length += 2
 	}
 
 	writeBoolean(value: boolean): void {
@@ -145,17 +189,48 @@ class CustomBuffer {
 	static fromBuffer(buffer: Buffer): CustomBuffer {
 		const newBuffer = CustomBuffer.alloc(buffer.length)
 
+		newBuffer.#length = buffer.length
+
 		newBuffer.copy(buffer)
 
 		return newBuffer
 	}
 
 	static alloc(length: number): CustomBuffer {
+		if (length <= 0)
+			throw Error('Cannot allocate buffer with less than 1 length')
+
 		const newBuffer = new CustomBuffer()
 
 		newBuffer.#buffer = Buffer.alloc(length)
 
+		newBuffer.#capacity = length
+		newBuffer.#length = 0
+
 		return newBuffer
+	}
+
+	expand(requiredEmptyLength: number) {
+		while (this.#capacity - this.#offset < requiredEmptyLength)
+			this.#capacity *= 2
+
+		const newBuffer = Buffer.alloc(this.#capacity)
+
+		this.#buffer.copy(newBuffer)
+
+		this.#buffer = newBuffer
+	}
+
+	checkExpand(requiredEmptyLength: number) {
+		if (this.#offset + requiredEmptyLength >= this.#capacity)
+			this.expand(requiredEmptyLength)
+	}
+
+	checkCanRead(byteLength: number) {
+		if (this.#offset + byteLength > this.#length)
+			throw new Error(
+				"Can't read attempting to access memory outside capacity bounds"
+			)
 	}
 
 	copy(originalBuffer: Buffer): void {
@@ -176,12 +251,16 @@ class CustomBuffer {
 		return this.#buffer.subarray(this.#offset)
 	}
 
-	get length(): number {
-		return this.#buffer.length
-	}
-
 	get buffer(): Buffer {
 		return this.#buffer
+	}
+
+	get capacity(): number {
+		return this.#capacity
+	}
+
+	get length(): number {
+		return this.#length
 	}
 
 	resetOffset(): void {

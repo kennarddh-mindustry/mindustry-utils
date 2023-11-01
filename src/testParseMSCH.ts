0189cc5c93609920cb2821985a1788069e9258cd
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-11-01 13:14:18 +0700

    feat: Add bread pfp

--- src/testParseMSCH.ts
+++ src/testParseMSCH.ts
@@ -36,7 +36,7 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 // console.log(schematic.tiles)
 
-const art = await loadImage(path.join(__dirname, '../data/TransparentImage.png'))
+const art = await loadImage(path.join(__dirname, '../data/bread.png'))
 
 const [canvas, ditherCanvas] = await GenerateSorterArtImage({
 	art,
